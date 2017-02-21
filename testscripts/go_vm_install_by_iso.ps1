0cfd26a43959c68242941dc5258ea4797b6d1be8
hhei <hhei@redhat.com>
2017-02-21 16:40:53 +0800

    Add testcase: install vm by iso

---
+++ testscripts/go_vm_install_by_iso.ps1
@@ -0,0 +1,114 @@
+###############################################################################
+##
+## Description:
+##   Check vm, because vm is already installed by iso, so only check vm exists
+##   Return passed, case is passed; return failed, case is failed
+##
+###############################################################################
+##
+## Revision:
+## v1.0 - hhei - 2/21/2017 - Check vm.
+##
+###############################################################################
+
+<#
+.Synopsis
+    reboot in vm.
+
+.Description
+    Check reboot in vm.
+
+.Parameter vmName
+    Name of the test VM.
+
+.Parameter testParams
+    Semicolon separated list of test parameters.
+#>
+
+param([String] $vmName, [String] $hvServer, [String] $testParams)
+#
+# Checking the input arguments
+#
+if (-not $vmName)
+{
+    "Error: VM name cannot be null!"
+    exit
+}
+
+if (-not $hvServer)
+{
+    "Error: hvServer cannot be null!"
+    exit
+}
+
+if (-not $testParams)
+{
+    Throw "Error: No test parameters specified"
+}
+
+#
+# Display the test parameters so they are captured in the log file
+#
+"TestParams : '${testParams}'"
+
+#
+# Parse the test parameters
+#
+$rootDir = $null
+$sshKey = $null
+$ipv4 = $null
+
+$params = $testParams.Split(";")
+foreach ($p in $params)
+{
+    $fields = $p.Split("=")
+    switch ($fields[0].Trim())
+    {
+    "sshKey"       { $sshKey = $fields[1].Trim() }
+    "rootDir"      { $rootDir = $fields[1].Trim() }
+    "ipv4"         { $ipv4 = $fields[1].Trim() }
+    default        {}
+    }
+}
+
+if (-not $rootDir)
+{
+    "Warn : no rootdir was specified"
+}
+else
+{
+    if ( (Test-Path -Path "${rootDir}") )
+    {
+        cd $rootDir
+    }
+    else
+    {
+        "Warn : rootdir '${rootDir}' does not exist"
+    }
+}
+
+#
+# Source the tcutils.ps1 file
+#
+. .\setupscripts\tcutils.ps1
+
+PowerCLIImport
+ConnectToVIServer $env:ENVVISIPADDR `
+                  $env:ENVVISUSERNAME `
+                  $env:ENVVISPASSWORD `
+                  $env:ENVVISPROTOCOL
+
+$result = $Failed
+$vmOut = Get-VMHost -Name $hvServer | Get-VM -Name $vmName
+if (-not $vmOut)
+{
+    Write-Error -Message "go_vm_install_by_iso: Unable to create VM object for VM $vmName" -Category ObjectNotFound -ErrorAction SilentlyContinue
+}
+else
+{
+    "Info : Find $vmName"
+    $result = $Passed
+}
+
+DisconnectWithVIServer
+return $result
