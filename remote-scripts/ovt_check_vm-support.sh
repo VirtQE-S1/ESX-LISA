e47b8be919c321226c036bc212b55650a7416ee1
ldu <ldu@redhat.com>
2017-03-17 18:16:05 +0800

    add new cases ovt_check_deploypkg /ovt_check_libmspack_installed /ovt_check_mount.vmhgfs /ovt_check_vm-support /ovt_yum_install_ovt

---
+++ remote-scripts/ovt_check_vm-support.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+###############################################################################
+##
+## Description:
+##   This script checks vm-support path after installed open-vm-tools.
+##   The vm-support should under /usr/bin/.
+##
+###############################################################################
+##
+## Revision:
+## v1.0 - ldu - 03/16/2017 - Draft script for case ESX-OVT-009.
+##
+###############################################################################
+
+dos2unix utils.sh
+
+# Source utils.sh
+. utils.sh || {
+    echo "Error: unable to source utils.sh!"
+    exit 1
+}
+
+# Source constants file and initialize most common variables
+UtilsInit
+
+#
+# Start the testing
+#
+
+if [[ $DISTRO != "redhat_7" ]]; then
+    SetTestStateSkipped
+    exit
+fi
+
+rpm -ql open-vm-tools |grep "/usr/bin/vm-support"
+if [[ $? == 0 ]]; then
+    LogMsg "Test successfully. There's vm-support file under /usr/bin/."
+    UpdateSummary "Test successfully. There's vm-support file under /usr/bin/."
+    SetTestStateCompleted
+    exit
+else
+    LogMsg "Test Failed. There's NO vm-support file under /usr/bin/."
+    UpdateSummary "Test failed. There's NO vm-support file under /usr/bin/."
+    SetTestStateFailed
+    exit 1
+fi
---
+++ remote-scripts/ovt_yum_install_ovt.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+###############################################################################
+##
+## Description:
+##   This script install the open-vm-tools.
+##   The open-vm-tools should be installed with yum command successfully.
+##
+###############################################################################
+##
+## Revision:
+##  v1.0 - ldu - 03/14/2017 - Draft script for case ESX-OVT-005.
+##
+###############################################################################
+
+dos2unix utils.sh
+
+# Source utils.sh
+. utils.sh || {
+    echo "Error: unable to source utils.sh!"
+    exit 1
+}
+
+# Source constants file and initialize most common variables
+UtilsInit
+
+#
+# Start the testing
+#
+
+if [[ $DISTRO != "redhat_7" ]]; then
+        SetTestStateSkipped
+        exit
+fi
+
+yum erase -y open-vm-tools-desktop
+yum erase -y open-vm-tools
+version=$(rpm -qa open-vm-tools)
+LogMsg "$version"
+if [ -n "$version" ]; then
+        LogMsg "unintall the open-vm-tools Failed"
+        UpdateSummary "Test Failed. unintall the open-vm-tools Failed."
+        SetTestStateAborted
+        exit 1
+else
+        yum -y install open-vm-tools-desktop
+        LogMsg "open-vm-tools installed successfully."
+        UpdateSummary " open-vm-tools installed successfully."
+        version=$(rpm -qa open-vm-tools)
+        if [ -n "$version" ]; then
+                LogMsg "open-vm-tools installed successfully."
+                UpdateSummary " open-vm-tools installed successfully."
+                SetTestStateCompleted
+                exit
+        else
+                LogMsg "open-vm-tools installed Failed."
+                UpdateSummary " open-vm-tools installed Failed."
+                SetTestStateFailed
+                exit 1
+        fi
+fi
