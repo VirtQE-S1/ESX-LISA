06266e46cf31c9b1b3e16cc953a13c68929e95ab
junfwang <junfwang@hyperv.redhat.com>
2017-10-26 16:26:50 +0800

    modify the scripts

--- testscripts/ovt_check_vmtools_status_synced_with_guest_stop_start.ps1
+++ testscripts/ovt_check_vmtools_status_synced_with_guest_stop_start.ps1
@@ -101,80 +101,77 @@ ConnectToVIServer $env:ENVVISIPADDR `
 
 $Result = $Failed
 $vmObj = Get-VMHost -Name $hvServer | Get-VM -Name $vmName
-if (-not $vmObj)
-{
+if (-not $vmObj){
     Write-Error -Message "CheckModules: Unable to create VM object for VM $vmName" -Category ObjectNotFound -ErrorAction SilentlyContinue
+    DisconnectWithVIServer
+    exit
 }
-else
-{ 
+
+#
+# main script code
+#
 
 # Get guest version
-    $DISTRO = ""
-    $modules_array = ""
-    $DISTRO = GetLinuxDistro ${ipv4} ${sshKey}
-    if ( $DISTRO -eq "RedHat7" )
-    {
-        $modules_array = $rhel7_modules.split(",")
+$DISTRO = ""
+$DISTRO = GetLinuxDistro ${ipv4} ${sshKey}
+if ( $DISTRO -eq "RedHat6" ){
+    Exit
+    $Result = $Skipped
+}
 
-        $vmtoolsStop = bin\plink.exe -i ssh\${sshKey} root@${ipv4} "systemctl stop vmtoolsd"
+bin\plink.exe -i ssh\${sshKey} root@${ipv4} "systemctl stop vmtoolsd"
         
-        $vmtoolsDeadInVm = bin\plink.exe -i ssh\${sshKey} root@${ipv4} "systemctl status vmtoolsd |grep dead"
-        
-        $vmtoolsDeadStatusInHost =  Get-VMHost -Name $hvServer | Get-VM -Name $vmName Get-View | Select Name,@{Name="toolstatus";Expression={$_.summary.guest.toolsRunningStatus}}
-        $vmtoolsDeadInHost = $null
-        if ($vmtoolsDeadStatusInHost.Name -eq $vmName){
-            $vmtoolsDeadInHost=$vmtoolsDeadStatusInHost.toolstatus
-                                     }                                  
-                                                    
-        $vmtoolsStart = bin\plink.exe -i ssh\${sshKey} root@${ipv4} "systemctl start vmtoolsd"
+$vmtoolsDeadInVm = bin\plink.exe -i ssh\${sshKey} root@${ipv4} "systemctl status vmtoolsd |grep dead"
 
-        $vmtoolsRunningInVm = bin\plink.exe -i ssh\${sshKey} root@${ipv4} "systemctl status vmtoolsd |grep running"
-    
-        $vmtoolsRunningStatusInHost = Get-VMHost -Name $hvServer | Get-VM -Name $vmName Get-View | Select Name,@{Name="toolstatus";Expression={$_.summary.guest.toolsRunningStatus}}
-        $vmtoolsRunningInHost = $null
-        if ($vmtoolsRunningStatusInHost.Name -eq $vmName){
-            $vmtoolsRunningInHost=$vmtoolsRunningStatusInHost.toolstatus
-                                 }
+Start-Sleep -Seconds 3
+        
+$vmtoolsDeadStatusInHost =  Get-VMHost -Name $hvServer | Get-VM -Name $vmName | Select-Object Name,@{Name="toolstatus";Expression={$_.ExtensionData.guest.toolsRunningStatus}}
+$vmtoolsDeadInHost = $null
+if ($vmtoolsDeadStatusInHost.Name -eq $vmName){
+    $vmtoolsDeadInHost=$vmtoolsDeadStatusInHost.toolstatus
+}                                  
+            
+bin\plink.exe -i ssh\${sshKey} root@${ipv4} "systemctl start vmtoolsd"
+
+
+
+$vmtoolsRunningInVm = bin\plink.exe -i ssh\${sshKey} root@${ipv4} "systemctl status vmtoolsd |grep running"
+
+Start-Sleep -Seconds 10
+
+$vmtoolsRunningStatusInHost = Get-VMHost -Name $hvServer  |Get-VM -Name $vmName  | Select-Object Name,@{Name="toolstatus";Expression={$_.EXtensionData.guest.toolsRunningStatus}}
+$vmtoolsRunningInHost = $null
+if ($vmtoolsRunningStatusInHost.Name -eq $vmName){
+    $vmtoolsRunningInHost=$vmtoolsRunningStatusInHost.toolstatus}
     
         
     
-        if ($vmtoolsDeadInVm -ne $null ){
-            if($vmtoolsDeadInHost -eq $guestToolsNotRunning ){
-                if($vmtoolsRunningInVm -ne $null){
-                    if($vmtoolsRunningInHost -eq $guestToolsRunning){
-                        Write-Output "Pass : vmtools status are the same when start and stop vmtoolsd"
-                        $Result=$Passed
-                    }
-                    else{   
-                        Write-Output "Error : after start vmtooldsd,vmtools status in host is not running"
-                        $Result=$Failed
-                    }
-                }
-                else{
-                    Write-Output "Error :after start vmtooldsd,vmtools status in vm is not running"
-                    $Result=$Aborted
-                }
+if ($vmtoolsDeadInVm -ne $null ){
+    if($vmtoolsDeadInHost -eq "guestToolsNotRunning" ){
+        if($vmtoolsRunningInVm -ne $null){
+            if($vmtoolsRunningInHost -eq "guestToolsRunning"){
+                Write-Output "Pass : vmtools status are the same when start and stop vmtoolsd"
+                $Result=$Passed
             }
-            else {
-                Write-Output "Error :after stop,vmtools status in host not the dead" 
-                $Result=$Aborted   
+            else{
+                 Write-Output "Error : after start vmtooldsd,vmtools status in host is not running"
+                 $Result=$Aborted
             }
         }
-        else
-        {
-            Write-Output "Error :after stop,vmtools status in vm not the dead"
+        else{
+            Write-Output "Error :after start vmtooldsd,vmtools status in vm is not running "
             $Result=$Aborted
         }
     }
-    else
-    {
-        "Error : Guest OS version is not RedHat7"
-        $Result = $Skiped
+    else {
+         Write-Output "Error :after stop,vmtools status in host not the dead" 
+         $Result=$Aborted
     }
-   
-
 }
-
+else{
+    Write-Output "Error :after stop,vmtools status in vm not the dead"
+    $Result=$Aborted
+}
 "Info : ovt_check_vmtools_status_synced_with_guest_stop_start.ps1 script completed"
 DisconnectWithVIServer
 return $Result
