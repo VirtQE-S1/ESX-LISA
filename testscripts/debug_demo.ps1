e4e6d9352329f647490ff684e83b6a9890f376c5
chinrw <chinqrw@gmail.com>
2018-09-11 16:13:09 +0800

    UPDATE: demo xml

--- testscripts/debug_demo.ps1
+++ testscripts/debug_demo.ps1
@@ -7,20 +7,34 @@
 ###############################################################################
 ##
 ## Revision:
-## v1.0 - xiaofwan - 11/25/2016 - Draft a template for Powershell script.
-## v1.1 - xiaofwan - 12/29/2016 - Add rootDir, sshKey, and ipv4 params support.
-## v1.2 - xiaofwan - 1/6/2017 - Move PowerCLIImport, ConnectToVIServer, and
+## v1.0.0 - xiaofwan - 11/25/2016 - Draft a template for Powershell script.
+## v1.1.0 - xiaofwan - 12/29/2016 - Add rootDir, sshKey, and ipv4 params support.
+## v1.2.0 - xiaofwan - 01/06/2017 - Move PowerCLIImport, ConnectToVIServer, and
 ##                              DisconnectWithVIServer functions to tcutils.ps1
-## v1.3 - xiaofwan - 1/9/2017 - Fix variable error bug.
+## v1.3.0 - xiaofwan - 01/09/2017 - Fix variable error bug.
 ##
 ###############################################################################
 
+
 <#
 .Synopsis
-    Demo script ONLY for test script.
+    What does this script.
 
 .Description
-    A demo for Powershell script as test script.
+    The example of setup xml
+    <test>
+        <testName>debug_demo</testName> # Name of test case
+        <testID>ESX-DEBUG-001</testID> # ID in automation document
+        <testScript>testscripts\debug_demo.ps1</testScript> # Name of test file
+        <files>remote-scripts/utils.sh</files> # file to use in remote guest
+        <testParams>
+            <param>TC_COVERED=RHEL6-38512,RHEL7-80221</param> # ID of test case
+        </testParams>
+        <RevertDefaultSnapshot>True</RevertDefaultSnapshot>
+        <timeout>600</timeout> # Timeout value
+        <onError>Continue</onError>
+        <noReboot>False</noReboot>
+    </test>
 
 .Parameter vmName
     Name of the test VM.
@@ -29,30 +43,27 @@
     Semicolon separated list of test parameters.
 #>
 
+
 param([String] $vmName, [String] $hvServer, [String] $testParams)
-#
 # Checking the input arguments
-#
-if (-not $vmName)
-{
+if (-not $vmName) {
     "Error: VM name cannot be null!"
-    exit
+    exit 1
 }
 
-if (-not $hvServer)
-{
+
+if (-not $hvServer) {
     "Error: hvServer cannot be null!"
-    exit
+    exit 1
 }
 
-if (-not $testParams)
-{
+
+if (-not $testParams) {
     Throw "Error: No test parameters specified"
 }
 
-#
+
 # Display the test parameters so they are captured in the log file
-#
 "TestParams : '${testParams}'"
 
 #
@@ -64,31 +75,25 @@ $ipv4 = $null
 $tcCovered = "undefined"
 
 $params = $testParams.Split(";")
-foreach ($p in $params)
-{
+foreach ($p in $params) {
     $fields = $p.Split("=")
-    switch ($fields[0].Trim())
-    {
-    "sshKey"       { $sshKey = $fields[1].Trim() }
-    "rootDir"      { $rootDir = $fields[1].Trim() }
-    "ipv4"         { $ipv4 = $fields[1].Trim() }
-    "TC_COVERED"   { $tcCovered = $fields[1].Trim() }
-    default        {}
+    switch ($fields[0].Trim()) {
+        "sshKey" { $sshKey = $fields[1].Trim() }
+        "rootDir" { $rootDir = $fields[1].Trim() }
+        "ipv4" { $ipv4 = $fields[1].Trim() }
+        "TC_COVERED" { $tcCovered = $fields[1].Trim() }
+        default {}
     }
 }
 
-if (-not $rootDir)
-{
+if (-not $rootDir) {
     "Warn : no rootdir was specified"
 }
-else
-{
-    if ( (Test-Path -Path "${rootDir}") )
-    {
+else {
+    if ( (Test-Path -Path "${rootDir}") ) {
         cd $rootDir
     }
-    else
-    {
+    else {
         "Warn : rootdir '${rootDir}' does not exist"
     }
 }
@@ -100,9 +105,9 @@ else
 
 PowerCLIImport
 ConnectToVIServer $env:ENVVISIPADDR `
-                  $env:ENVVISUSERNAME `
-                  $env:ENVVISPASSWORD `
-                  $env:ENVVISPROTOCOL
+    $env:ENVVISUSERNAME `
+    $env:ENVVISPASSWORD `
+    $env:ENVVISPROTOCOL
 
 ###############################################################################
 #
