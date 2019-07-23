79edc83d6985096890d8d9d80b9762bd2e483cff
boyang <yangboroy@163.com>
2019-07-23 11:28:49 +0800

    fix(vsock): Resolve sshpass installation

--- remote-scripts/vsocket_guest_as_server_cid.sh
+++ remote-scripts/vsocket_guest_as_server_cid.sh
@@ -33,24 +33,38 @@ hv_server=$1
 # TODO. HERE. Test $1
 
 # Install sshpass with git
-git clone git://github.com/kevinburke/sshpass.git
-cd sshpass
-./configure
-make && make install
-# TODO. HERE. Test instalaltion status
+LogMsg "INFO: Will install sshpass in $DISTRO"
+UpdateSummary "INFO: Will install sshpass in $DISTRO"
+if [ "$DISTRO" == "redhat_7" ]; then
+
+    url=http://download.eng.bos.redhat.com/brewroot/vol/rhel-7/packages/sshpass/1.06/2.el7/x86_64/sshpass-1.06-2.el7.x86_64.rpm
+elif [ "$DISTRO" == "redhat_8" ]; then
+
+    url=http://download.eng.bos.redhat.com/brewroot/vol/rhel-8/packages/sshpass/1.06/2.el8/x86_64/sshpass-1.06-2.el8.x86_64.rpm
+else
+    url=http://download.eng.bos.redhat.com/brewroot/vol/rhel-6/packages/sshpass/1.06/1.el6/x86_64/sshpass-1.06-1.el6.x86_64.rpm
+fi
+yum install -y $url
+if [[ $? -eq 0 ]]; then
+    LogMsg "ERROR: Install sshpass failed"
+    UpdateSummary "ERROR: Install sshpass failed"
+    SetTestStateFailed
+    exit 1
+fi
 
 # SCP server bin to hv server
 sshpass -p 123qweP scp -o StrictHostKeyChecking=no /root/client root@$hv_server:/tmp/
+sshpass -p 123qweP ssh -o StrictHostKeyChecking=no /root/client root@$hv_server "chmod a+x /tmp/server"
 # TODO. HERE. Test its scp result
 
 # Execute it in VM as a server
-./root/server &
+/root/server &
 ports=`cat /root/port.txt`
 LogMsg "DEBUG: ports: $ports"
 UpdateSummary "DEBUG: ports: $ports"
 
 # Execute it in hv server as a guest
-sshpass -p 123qweP ssh -o StrictHostKeyChecking=no root@$hv_server "./tmp/client $ports"
+sshpass -p 123qweP ssh -o StrictHostKeyChecking=no root@$hv_server "/tmp/client $ports"
 if [[ $? -eq 0 ]]; then
     LogMsg "INFO: ESXi Host as a guest communicates with VM as a server well"
     UpdateSummary "INFO: ESXi Host as a guest communicates with VM as a server well"
--- remote-scripts/vsocket_host_as_server_cid.sh
+++ remote-scripts/vsocket_host_as_server_cid.sh
@@ -35,21 +35,35 @@ hv_server=$1
 # TODO. HERE. Test $1
 
 # Install sshpass with git
-git clone git://github.com/kevinburke/sshpass.git
-cd sshpass
-./configure
-make && make install
-# TODO. HERE. Test instalaltion status
+LogMsg "INFO: Will install sshpass in $DISTRO"
+UpdateSummary "INFO: Will install sshpass in $DISTRO"
+if [ "$DISTRO" == "redhat_7" ]; then
+
+    url=http://download.eng.bos.redhat.com/brewroot/vol/rhel-7/packages/sshpass/1.06/2.el7/x86_64/sshpass-1.06-2.el7.x86_64.rpm
+elif [ "$DISTRO" == "redhat_8" ]; then
+
+    url=http://download.eng.bos.redhat.com/brewroot/vol/rhel-8/packages/sshpass/1.06/2.el8/x86_64/sshpass-1.06-2.el8.x86_64.rpm
+else
+    url=http://download.eng.bos.redhat.com/brewroot/vol/rhel-6/packages/sshpass/1.06/1.el6/x86_64/sshpass-1.06-1.el6.x86_64.rpm
+fi
+yum install -y $url
+if [[ $? -eq 0 ]]; then
+    LogMsg "ERROR: Install sshpass failed"
+    UpdateSummary "ERROR: Install sshpass failed"
+    SetTestStateFailed
+    exit 1
+fi
 
 # SCP server bin to hv server
 sshpass -p 123qweP scp -o StrictHostKeyChecking=no /root/server root@$hv_server:/tmp/
+sshpass -p 123qweP ssh -o StrictHostKeyChecking=no /root/server root@$hv_server "chmod a+x /tmp/server"
 # TODO. HERE. Test its scp result
 
 # Execute it in ESXi Host as a server
-sshpass -p 123qweP ssh -o StrictHostKeyChecking=no root@$hv_server "./tmp/server"
+sshpass -p 123qweP ssh -o StrictHostKeyChecking=no root@$hv_server "/tmp/server"
 
 # Execute it in a VM as a guest
-./root/client 2
+/root/client 2
 if [[ $? -eq 0 ]]; then
     LogMsg "INFO: ESXi Host as a server communicates with VM as a clinet well"
     UpdateSummary "INFO: ESXi Host as a server communicates with VM as a clinet well"
