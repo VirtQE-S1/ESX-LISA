37706b1a9f0cf25654684a6c1678d07976f7fc9e
ldu <ldu@redhat.com>
2020-01-12 17:21:34 +0800

    update cases fips enable

--- testscripts/go_fips_enable.ps1
+++ testscripts/go_fips_enable.ps1
@@ -3,30 +3,27 @@
 ## RHEL-182794 - [general operation] Boot up guest with fips enabled
 ##
 ## Revision:
-##  v1.0.0 - ldu - 01/10/2020 - Build the script
+##  v1.0.0 - ldu - 01/12/2020 - Build the script
 ########################################################################################
 
 
 <#
 .Synopsis
-   
-[cloud-init]Reboot guest and check IP status after case RHEL-137083 Customize Guest uses static IP with cloud-init
+   [general operation] Boot up guest with fips enabled
 .Description
 <test>
-            <testName>cloud-init_reboot_customization_static</testName>
-            <testID>ESX-cloud-init-006</testID>
-            <setupScript>setupscripts\add_vmxnet3.ps1</setupScript>
-            <testScript>testscripts/cloud-init_reboot_customization_static.ps1</testScript  >
-            <files>remote-scripts/utils.sh</files>
-            <testParams>
-                <param>nicName=auto-test</param>
-                <param>TC_COVERED=RHEL6-0000,RHEL-153099</param>
-            </testParams>
-            <RevertDefaultSnapshot>True</RevertDefaultSnapshot>
-            <timeout>1800</timeout>
-            <onError>Continue</onError>
-            <noReboot>False</noReboot>
-        </test>
+        <testName>go_fips_enable</testName>
+        <testID>ESX-go-021</testID>
+        <testScript>testscripts/go_fips_enable.ps1</testScript>
+        <files>remote-scripts/utils.sh</files>
+        <testParams>
+            <param>TC_COVERED=RHEL6-0000,RHEL-182794</param>
+        </testParams>
+        <RevertDefaultSnapshot>True</RevertDefaultSnapshot>
+        <timeout>1200</timeout>
+        <onError>Continue</onError>
+        <noReboot>False</noReboot>
+</test>
 
 .Parameter vmName
     Name of the test VM.
@@ -147,9 +144,11 @@ if ($DISTRO -ne "RedHat7"-and $DISTRO -ne "RedHat8"-and $DISTRO -ne "RedHat6") {
     return $Skipped
 }
 
+
 #Get boot disk and UUID
-$bootDisk = bin\plink.exe -i ssh\${sshKey} root@${ipv4} "df /boot |sed -n "2, 1p" |awk '{print $1}'"
-if ($null -eq $staticIP)
+$command = "df /boot | grep boot | awk '{print `$1}'"
+$bootDisk = Write-Output y | bin\plink.exe -i ssh\${sshKey} root@${ipv4} $command
+if ($null -eq $bootDisk)
 {
     Write-Host -F Red " Failed: Failed get the boot disk, $bootDisk"
     Write-Output " Failed: Failed get the boot disk, $bootDisk"
@@ -162,7 +161,8 @@ else
 }    
 
 #Get boot disk's UUID
-$uuid = bin\plink.exe -i ssh\${sshKey} root@${ipv4} "blkid $bootDisk |awk '{print $2}'"
+$command = "blkid $bootDisk |awk '{print `$2}'"
+$uuid = Write-Output y | bin\plink.exe -i ssh\${sshKey} root@${ipv4} $command
 if ($null -eq $uuid)
 {
     Write-Host -F Red " Failed:  the boot disk's UUID is $uuid"
@@ -177,15 +177,16 @@ else
 
 #cofigure fips enable in guest
 $Command = "yum install dracut-fips dracut-fips-aesni -y && dracut -v -f && grubby --update-kernel=ALL --args='boot=$uuid' && grubby --update-kernel=ALL --args='fips=1'"
-$status = SendCommandToVM $ipv4 $sshkey $command
-if (-not $status) {
+$status = Write-Output y | bin\plink.exe -i ssh\${sshKey} root@${ipv4} $command
+if (-not $status) 
+{
     LogPrint "ERROR : configure fips commands failed"
     $retVal = $Aborted
 }
-else {
-       LogPrint "Pass : configure fips commands passed"
+else
+{
+    LogPrint "Pass : configure fips commands passed"
 }
-
 
 #reboot cloned guest
 $reboot = bin\plink.exe -i ssh\${sshKey} root@${ipv4} 'reboot'
@@ -209,20 +210,19 @@ else
     return $Aborted
 }
 
-
-#Check the static IP after reboot guest
+#Check the fips value after reboot guest
 $fips = bin\plink.exe -i ssh\${sshKey} root@${ipv4} "cat /proc/sys/crypto/fips_enabled"
 if ($fips -eq "1")
 {
-    Write-Host -F Red " Failed:  dddthe customization gust Failed with static IP for second NIC $fips"
-    Write-Output " Failed:  dddthe customization gust Failed with static IP for second NIC $fips"
+    Write-Host -F Red " passed, the fips enabled with  /proc/sys/crypto/fips_enabled  $fips"
+    Write-Output " passed, the fips enabled with fips  /proc/sys/crypto/fips_enabled $fips"
     $retVal = $Passed
 }
 else
 {
     
-    Write-Host -F Red " Passed:  ccthe customization gust passed with static IP for second NIC $fips"
-    Write-Output " Passed:  ccthe customization gust passed withstatic IP for second NIC $fips"
+    Write-Host -F Red " Failed:  fips value is $fips"
+    Write-Output " Failed: fips value is  $fips"
     return $Failed
 }    
 
